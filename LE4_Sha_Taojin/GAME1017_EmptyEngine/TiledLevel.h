f85c38a95cf283222b61cba89c5f65e61f33b7c2
shataojin <stj2062287@gmail.com>
2022-03-25 20:41:40 -0400

    finished code in tiledlevel

--- LE4_Sha_Taojin/GAME1017_EmptyEngine/TiledLevel.h
+++ LE4_Sha_Taojin/GAME1017_EmptyEngine/TiledLevel.h
@@ -11,16 +11,40 @@
 
 class Tile : public SpriteObject
 {
+public:
+	Tile(SDL_Rect s, SDL_FRect d , bool o = false, bool h= false)
+		:SpriteObject(s,d),m_obstacle(o), m_hazard(h) {}
+	Tile* Clone() {
+		return new Tile(m_src,m_dst, m_obstacle, m_hazard);
+	}
+	void Update(){}
+	void Render(){}
+	bool IsObstacle() { return m_obstacle; }
+	void SetXY(float x, float y) { m_dst.x = x; m_dst.y = y; }
+
+private:
+	bool m_obstacle, m_hazard;
 
 };
 
 class TiledLevel : public GameObject
 {
 public:
-	
+	TiledLevel(const unsigned short r, const unsigned short c,
+		const int w, const int h, const char* tileData, const char* levelData,
+		const char* tileKey);
+		~TiledLevel();
+		void Update(){}
+		void Render();
+		vector<Tile*>& GetObstacles() { return m_obstacles; }
+
 private:
 	
+	const char* m_tileKey;
+	int m_rows, m_cols;
+	map<char, Tile*>m_tiles;
+	vector<vector<Tile*>>m_level;
+	vector<Tile*>m_obstacles;
 };
 
 #endif
-
